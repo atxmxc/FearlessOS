e29f1854a0c38bf1d47fa29792e9b8e40d632a1b
Andrew Petreanu <andrew2022andrew149@gmail.com>
2026-01-11 22:05:42 +0000

    Finished Basic Launcher, need to add links

--- source/main.c
+++ source/main.c
@@ -1,25 +1,74 @@
-/*---------------------------------------------------------------------------------
-
-	Basic template code for starting a DS app
-
----------------------------------------------------------------------------------*/
 #include <nds.h>
 #include <stdio.h>
 
-//---------------------------------------------------------------------------------
-int main(int argc, char* argv[]) {
-//---------------------------------------------------------------------------------
+PrintConsole top;
+PrintConsole bottom;
 
-	consoleDemoInit();
-	iprintf("Hello World!");
+int option = 0;
 
-	while(pmMainLoop()) {
-		swiWaitForVBlank();
-		scanKeys();
-		int pressed = keysDown();
-		if(pressed & KEY_START) break;
-	}
+void drawMenu()
+{
+    consoleSelect(&bottom);
+    consoleClear();
 
-	return 0;
+    iprintf("\n\n    FEARLESS OS\n");
+    iprintf("  Use LEFT / RIGHT\n");
+    iprintf("     Press A\n\n");
 
+    if (option == 0)
+        iprintf("    [ R4 MENU ]   CUSTOM MENU");
+    else
+        iprintf("      R4 MENU   [ CUSTOM MENU ]");
+}
+
+int main(int argc, char* argv[])
+{
+    powerOn(POWER_ALL);
+
+    // TOP screen
+    videoSetMode(MODE_0_2D);
+    vramSetBankA(VRAM_A_MAIN_BG);
+
+    consoleInit(&top, 0, BgType_Text4bpp, BgSize_T_256x256, 31, 0, true, true);
+
+    // BOTTOM screen
+    videoSetModeSub(MODE_0_2D);
+    vramSetBankC(VRAM_C_SUB_BG);
+
+    consoleInit(&bottom, 0, BgType_Text4bpp, BgSize_T_256x256, 30, 0, false, true);
+
+    // Draw top screen
+    consoleSelect(&top);
+    consoleClear();
+    iprintf("\n\n   FEARLESS OS");
+    iprintf("\n   System Ready");
+
+    // Draw bottom menu
+    drawMenu();
+
+    while (1)
+    {
+        scanKeys();
+        int keys = keysDown();
+
+        if (keys & KEY_LEFT) {
+            option = 0;
+            drawMenu();
+        }
+
+        if (keys & KEY_RIGHT) {
+            option = 1;
+            drawMenu();
+        }
+
+        if (keys & KEY_A) {
+            consoleSelect(&bottom);
+            consoleClear();
+            iprintf("\n\n   Launching...");
+        }
+
+        swiWaitForVBlank();
+    }
+
+    return 0;
 }
