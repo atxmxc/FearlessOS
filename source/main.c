6a778f77e88dbae2aa632f5f47a29555cd33b410
Andrew Petreanu <andrew2022andrew149@gmail.com>
2026-01-11 22:15:21 +0000

    Finished Basic UI and Controls

--- source/main.c
+++ source/main.c
@@ -1,74 +1,69 @@
 #include <nds.h>
 #include <stdio.h>
 
-PrintConsole top;
-PrintConsole bottom;
+PrintConsole topScreen;
+PrintConsole bottomScreen;
 
 int option = 0;
 
-void drawMenu()
+void drawTopBar()
 {
-    consoleSelect(&bottom);
+    consoleSelect(&topScreen);
     consoleClear();
 
-    iprintf("\n\n    FEARLESS OS\n");
-    iprintf("  Use LEFT / RIGHT\n");
-    iprintf("     Press A\n\n");
-
-    if (option == 0)
-        iprintf("    [ R4 MENU ]   CUSTOM MENU");
-    else
-        iprintf("      R4 MENU   [ CUSTOM MENU ]");
+    iprintf("\x1b[2;0H################################");
+    iprintf("\x1b[3;0H#                              #");
+    iprintf("\x1b[4;0H#      FEARLESS  LAUNCHER      #");
+    iprintf("\x1b[5;0H#                              #");
+    iprintf("\x1b[6;0H################################");
 }
 
-int main(int argc, char* argv[])
+void drawBottomMenu()
+{
+    consoleSelect(&bottomScreen);
+    consoleClear();
+
+    iprintf("\n\n           FEARLESS OS\n");
+    iprintf("       LEFT/RIGHT to select\n");
+    iprintf("           A to launch\n\n");
+
+    if (option == 0)
+        iprintf("   [ R4 MENU ]    FEARLESS");
+    else
+        iprintf("     R4 MENU    [ FEARLESS ]");
+}
+
+int main(void)
 {
     powerOn(POWER_ALL);
 
-    // TOP screen
     videoSetMode(MODE_0_2D);
-    vramSetBankA(VRAM_A_MAIN_BG);
-
-    consoleInit(&top, 0, BgType_Text4bpp, BgSize_T_256x256, 31, 0, true, true);
-
-    // BOTTOM screen
     videoSetModeSub(MODE_0_2D);
+
+    vramSetBankA(VRAM_A_MAIN_BG);
     vramSetBankC(VRAM_C_SUB_BG);
 
-    consoleInit(&bottom, 0, BgType_Text4bpp, BgSize_T_256x256, 30, 0, false, true);
+    consoleInit(&topScreen, 0, BgType_Text4bpp, BgSize_T_256x256, 31, 0, true, true);
+    consoleInit(&bottomScreen, 0, BgType_Text4bpp, BgSize_T_256x256, 30, 0, false, true);
 
-    // Draw top screen
-    consoleSelect(&top);
-    consoleClear();
-    iprintf("\n\n   FEARLESS OS");
-    iprintf("\n   System Ready");
-
-    // Draw bottom menu
-    drawMenu();
+    drawTopBar();
+    drawBottomMenu();
 
     while (1)
     {
         scanKeys();
         int keys = keysDown();
 
-        if (keys & KEY_LEFT) {
-            option = 0;
-            drawMenu();
-        }
+        if (keys & KEY_LEFT)  { option = 0; drawBottomMenu(); }
+        if (keys & KEY_RIGHT) { option = 1; drawBottomMenu(); }
 
-        if (keys & KEY_RIGHT) {
-            option = 1;
-            drawMenu();
-        }
-
-        if (keys & KEY_A) {
-            consoleSelect(&bottom);
+        if (keys & KEY_A)
+        {
+            consoleSelect(&bottomScreen);
             consoleClear();
             iprintf("\n\n   Launching...");
         }
 
         swiWaitForVBlank();
     }
-
-    return 0;
 }
